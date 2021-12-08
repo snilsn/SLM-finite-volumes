ef289b6f70bc6ea6720175befc1a0663acca9d2f
nilspf <nilspfeifer2010@googlemail.com>
2021-12-08 21:28:06 +0100

    larger layer size

--- parameters.py
+++ parameters.py
@@ -1,18 +1,20 @@
 from fipy import numerix
 
-L_x = 0.0005
+L_x = 0.0004
 L_y = 0.0002
-L_z = 0.00006
+L_z = 0.0002
 
-N_x = 25
+N_x = 30
 N_y = 25
-N_z = 25
+N_z = 30
 
 dx = L_x / N_x
 dy = L_y / N_y
 dz = L_z / N_z
 
 packing_efficiency = 0.6
+
+T_A = 300.0
 
 g = 9.81
 sigma = 5.670374419e-8
@@ -38,11 +40,11 @@ steel_cond_fluid = 30.0 #???
 laser_power = 60.0
 beam_radius = 50e-6
 distribution_factor = 1.5
-scanning_speed = 1.0 #250 mm per second
+scanning_speed = 1.0 #1000 mm per second
 
 N_IH = laser_power*steel_liquid_absoprtion/(numerix.pi*beam_radius**2*scanning_speed)/(steel_density_liquid*steel_fluid_c*(steel_T_L-300)+steel_density_liquid*steel_latent_heat)
 
-layers = 5
+layers = 6
 hatches = 5
 
 layer_thickness = L_z / layers
