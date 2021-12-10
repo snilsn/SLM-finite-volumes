feebc960a0e3a8d1656e089884c6714a2314b6be
nilspf <nilspfeifer2010@googlemail.com>
2021-12-10 17:24:06 +0100

    nonuniform y and z grid started

---
+++ helper_functions.py
@@ -0,0 +1,30 @@
+import numpy as np
+from fipy import numerix
+
+def create_dz(n, m, L):
+    
+    faces = numerix.array([L*(i/n)**m for i in range(n+1)])
+    dz = numerix.diff(faces)
+    if numerix.absolute(dz.sum() - L) < 1e-3:
+        return dz
+    else:
+        print('error, wrong sum')
+        return None
+    
+def create_dy(n, m, L):
+
+    faces1 = numerix.array([L/2*(i/round(n/2))**m for i in range(round(n/2)+1)])
+    faces2 = L - faces1
+    faces2.sort()
+    faces2 = faces2[1:]
+
+    faces = numerix.concatenate((faces1, faces2))
+    dy = numerix.diff(faces)
+    
+    print(dy.sum())
+    print(L)
+    if numerix.absolute(dy.sum() - L) < 1e-3:
+        return dy
+    else:
+        print('error, wrong sum')
+        return None        
